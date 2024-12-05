b0c9010dad3f39981f34ce0558a0614b71df632a
Alexis <2175497@cegepmontpetit.ca>
2024-12-05 11:36:58 -0500

    FCT Attack
    Faire attaquer le slimey

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {transition, trigger, useAnimation} from "@angular/animations";
-import {shakeX} from "ng-animate";
+import {shakeX, pulse, jello} from "ng-animate";
 
 @Component({
   selector: 'app-root',
@@ -8,11 +8,15 @@ import {shakeX} from "ng-animate";
   styleUrls: ['./app.component.css'],
   animations: [
     trigger('death', [transition(':increment', useAnimation(shakeX, {params: {timing: 0.5}}))]),
+    trigger('attack', [transition(':increment', useAnimation(pulse, {params: {timing: 0.3, scale: 4.5}}))]),
+    trigger('preAttack', [transition(':increment', useAnimation(jello, {params: {timing: 0.5}}))]),
 ]
 })
 export class AppComponent {
   slimeIsPresent = false;
   ng_death = 0;
+  ng_preAttack = 0;
+  ng_attack = 0;
 
   constructor() {
   }
@@ -33,6 +37,8 @@ export class AppComponent {
 
   attack(){
     // TODO Jouer une animation et augmenter l'intensité du mouvement avec scale
+    this.ng_preAttack++;
+    setTimeout(() => {this.ng_attack++}, 200);
     // TODO Jouer une autre animation avant
   }
 
